23cb5eed3ee1ae22862ed2efd88631300e93b500
lyang <lyang@10.10.122.101>
2019-09-18 11:32:56 -0400

    art and processing

--- Leo,Yang/src/demo/Demo.java
+++ Leo,Yang/src/demo/Demo.java
@@ -1,5 +1,38 @@
 package demo;
 
-public class Demo {
+import Processing.core.PApplet;
 
+public class Demo extends PApplet
+{
+	
+public static void amin(String[] args)
+{
+	PApplet.main("demo.Demo");
+}
+
+private int centerX;
+
+private int centerXy;
+
+public void settings()
+{
+	size(800,600);
+	}
+
+	public void setup()
+	{
+		centerX = 400;
+	}
+
+	public void draw()
+	{
+		background(73, 219, 245);
+		
+		ellipse(centerX,300,200,200);
+		
+		centerX -= 1;
+		
+		
+	}
+	}
 }
